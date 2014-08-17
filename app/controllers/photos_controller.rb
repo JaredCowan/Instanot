2531d1add4d85a2c3e954f3a4ba994efd4e6dc37
JaredCowan <jaredlc@outlook.com>
2014-08-17 15:12:36 -0700

    Login and Signup form styled and working. Added background styled div containers to image feed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,19 @@
 class PhotosController < ApplicationController
   def index
     @photos = Photo.all.reverse
+    redirect_to login_path if !current_user
   end
 
   def new
     @photo =Photo.new
+    redirect_to login_path if !current_user
   end
   
   def create
     # Find our parent decision that we should attach to
     @photo = current_user.photos.new(photo_params)
     @photo.date ||= DateTime.now
+    redirect_to login_path if !current_user
     # Attach this criterion to a decision
     if @photo.save
       redirect_to photos_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 	def index
 		@users = User.all
+		redirect_to login_path if !current_user
 	end
 
 	def new
 		@user = User.new
+		# redirect_to login_path if !current_user
 	end
 
 	def create
@@ -12,6 +14,7 @@ class UsersController < ApplicationController
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to root_url, notice: "Thank you for signing up!"
+			# redirect_to login_path if !current_user
 		else
 			render "new"
 		end
@@ -19,6 +22,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		redirect_to login_path if !current_user
 	end
 
 	private
