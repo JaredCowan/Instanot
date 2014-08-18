ccf26f794c6c26c9701f62ef85271c6a0b59d49f
JaredCowan <jaredlc@outlook.com>
2014-08-17 19:40:08 -0700

    Trying to fix signup error and file type on deployment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to root_url, notice: "Thank you for signing up!"
+			redirect_to photos_path, notice: "Thank you for signing up!"
 		else
 			render "new"
 		end
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+		params.require(:user).permit(:email, :password, :password_confirmation, :username)
 	end
 end
