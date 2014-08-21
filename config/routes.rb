37539a211f67f5b87824167d30793e8e3660cc72
JaredCowan <jaredlc@outlook.com>
2014-08-21 15:42:06 -0700

    Changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Paperclipfun::Application.routes.draw do
 	get 'signup', to: 'users#new',        as: 'signup'
 	get 'login',  to: 'sessions#new',     as: 'login'
-	get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
 	resources :sessions
 	resources :photos
 	resources :users
-
 	root 'photos#index'
 end
